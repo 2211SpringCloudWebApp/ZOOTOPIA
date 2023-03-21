95c918463c837ccc92957d6fac29734decb5066a
WhoAmI_1024 <jhoon2.1024@gmail.com>
2023-03-21 16:29:13 +0900

    230321분 merged

--- src/main/java/com/kh/zootopia/comment/controller/CommentController.java
+++ src/main/java/com/kh/zootopia/comment/controller/CommentController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.zootopia.comment.domain.Comment;
-import com.kh.zootopia.comment.domain.PageInfo;
 import com.kh.zootopia.comment.service.CommentService;
+import com.kh.zootopia.review.domain.PageInfo;
 
 @Controller
 public class CommentController {
